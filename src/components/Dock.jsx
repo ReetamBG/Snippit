3673abf497043da175bd9729e4744a12b95aa002
ReetamBG <r33t4m@gmail.com>
2025-04-25 23:12:52 +0530

    need some changes on the image upload maybe use cloudinary instead of upload thing

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 const Dock = async () => {
   const user = await getUserByClerkId()
   return (
-    <div className="w-full flex justify-center md:hidden sticky bottom-3">
+    <div className="w-full flex justify-center md:hidden sticky bottom-2">
       <div className="flex justify-around w-[80%] bg-white dark:bg-background rounded-full p-3 border-1 border-gray-800 dark:border-gray-500">
         <Link href="/"><Home className="hover:scale-105 cursor-pointer size-5" /></Link>
         <Link href="/notifications" ><Bell className="hover:scale-105 cursor-pointer size-5" /></Link>
