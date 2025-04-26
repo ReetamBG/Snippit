1d03b2569b7979c260573bf05eeb3424653074bf
ReetamBG <r33t4m@gmail.com>
2025-04-26 15:37:36 +0530

    Some changes to ui

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -7,8 +7,8 @@ import Link from 'next/link'
 const Dock = async () => {
   const user = await getUserByClerkId()
   return (
-    <div className="w-full flex justify-center md:hidden sticky bottom-1">
-      <div className="flex justify-around w-[50%] bg-white dark:bg-background rounded-full p-3 border-1 border-gray-800 dark:border-gray-500">
+    <div className="w-full flex justify-center md:hidden sticky bottom-1 ">
+      <div className="flex justify-around w-[60%] backdrop-blur-lg bg-white/30 dark:bg-black/30 rounded-full p-3 border-1 border-gray-800 dark:border-gray-500">
         <Link href="/"><Home className="hover:scale-105 cursor-pointer size-5" /></Link>
         <Link href="/notifications" ><Bell className="hover:scale-105 cursor-pointer size-5" /></Link>
         <Link href={`/profile/${user?.username}`}><User className="hover:scale-105 cursor-pointer size-5" /></Link>
